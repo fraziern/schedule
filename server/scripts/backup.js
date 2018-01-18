8d840f44b463a9c4c58f7609d7e9c6658503a841
Nick Frazier <nrflaw@gmail.com>
2018-01-18 09:22:06 -0500

    bug fix

--- server/scripts/backup.js
+++ server/scripts/backup.js
@@ -2,37 +2,42 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 var dbconnect = require("../db_connect");
-var Promise = require("bluebird");
+// var Promise = require("bluebird");
 
 dbconnect();
 
 var DateCard = require("../models/DateCard");
 
-var backupSchema = new Schema({
-  backup: [Schema.Types.Mixed]
-}, {
-  timestamps: true  // adds createdAt and updatedAt
-});
+var backupSchema = new Schema(
+  {
+    backup: [Schema.Types.Mixed]
+  },
+  {
+    timestamps: true // adds createdAt and updatedAt
+  }
+);
 
 var Backup = mongoose.model("Backup", backupSchema);
 
 var createPromise = new Promise(function(resolve, reject) {
-  DateCard.find().lean().exec(function(err, datecards) {
-    if (err) reject("find error");
-    else {
-      Backup.create({ backup: datecards }, function (err) {
-        if (err) reject("create error");
-        else resolve("Created backup.");
-      });
-    }
-  });
+  DateCard.find()
+    .lean()
+    .exec(function(err, datecards) {
+      if (err) reject("find error");
+      else {
+        Backup.create({ backup: datecards }, function(err) {
+          if (err) reject("create error");
+          else resolve("Created backup.");
+        });
+      }
+    });
 });
 
 var deletePromise = new Promise(function(resolve, reject) {
   // delete any backups older than 5 days
   var weekOld = new Date();
   weekOld.setDate(weekOld.getDate() - 5);
-  Backup.remove({ createdAt: {$lt: weekOld }}, function (err) {
+  Backup.remove({ createdAt: { $lt: weekOld } }, function(err) {
     if (err) reject("delete error");
     else resolve("Deleted old stuff.");
   });
@@ -48,4 +53,4 @@ Promise.all([createPromise, deletePromise]).then(function(values) {
 setTimeout(function() {
   console.log("Timed out.");
   process.exit(0);
-},15000);
+}, 15000);
